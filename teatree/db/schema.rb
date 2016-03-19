02310656426a3b842ed0f4b62f3fbcb09936cddb
SatoTakuya <satotakuya@MacBook-Air-3.local>
2016-03-19 19:17:29 +0900

    productsテーブル作成

--- teatree/db/schema.rb
+++ teatree/db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319095213) do
+ActiveRecord::Schema.define(version: 20160319101522) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "species",     limit: 255
+    t.text     "explanation", limit: 65535
+    t.integer  "gram",        limit: 4
+    t.string   "temperature", limit: 255
+    t.integer  "time",        limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "image",       limit: 65535
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
