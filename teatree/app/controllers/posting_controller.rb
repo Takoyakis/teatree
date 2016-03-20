90af14c01f20fd6845bc7f077c3dbef7b4837216
SatoTakuya <satotakuya@MacBook-Air-3.local>
2016-03-20 21:04:42 +0900

    投稿のストロングパラメーターと投稿完了の画面を作った

--- teatree/app/controllers/posting_controller.rb
+++ teatree/app/controllers/posting_controller.rb
@@ -1,11 +1,15 @@
 class PostingController < ApplicationController
 
   def create
-    Posting.create()
+    Posting.create(name: posting_params[:name], gram: posting_params[:gram], temperature: posting_params[:temperature], time: posting_params[:time], experience: posting_params[:experience], coment: posting_params[:coment])
   end
 
   def new
   end
 
 
+  private
+  def posting_params
+    params.permit(:name, :gram, :temperature, :time, :experience, :coment)
+  end
 end
