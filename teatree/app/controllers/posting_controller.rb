f30fe6706b9e1b04137650a878450f3f89d7d2c2
SatoTakuya <satotakuya@MacBook-Air-3.local>
2016-03-21 20:16:34 +0900

    ログインしていないと投稿できないようにしたりした

--- teatree/app/controllers/posting_controller.rb
+++ teatree/app/controllers/posting_controller.rb
@@ -1,7 +1,9 @@
 class PostingController < ApplicationController
 
+  before_action :authenticate_user!, only: :new
+
   def create
-    Posting.create(name: posting_params[:name], gram: posting_params[:gram], temperature: posting_params[:temperature], time: posting_params[:time], experience: posting_params[:experience], coment: posting_params[:coment])
+    Posting.create(category_id: posting_params[:category_id], name: posting_params[:name], gram: posting_params[:gram], temperature: posting_params[:temperature], time: posting_params[:time], experience: posting_params[:experience], coment: posting_params[:coment])
   end
 
   def new
@@ -10,10 +12,11 @@ class PostingController < ApplicationController
   def index
     @postings = Posting.all.limit(12)
   end
-  
+
 
   private
   def posting_params
-    params.permit(:name, :gram, :temperature, :time, :experience, :coment)
+    params.permit(:category_id, :name, :gram, :temperature, :time, :experience, :coment)
   end
+
 end
