c6c39353224cb46c1b17a55837b280de89077ff8
SatoTakuya <satotakuya@MacBook-Air-3.local>
2016-03-24 14:56:10 +0900

    postingのエラー解消

--- teatree/app/controllers/posting_controller.rb
+++ teatree/app/controllers/posting_controller.rb
@@ -3,7 +3,7 @@ class PostingController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def create
-    Posting.create(category_id: posting_params[:category_id], name: posting_params[:name], gram: posting_params[:gram], temperature: posting_params[:temperature], time: posting_params[:time], experience: posting_params[:experience], coment: posting_params[:coment])
+    Posting.create(category_id: posting_params[:category_id], name: posting_params[:name], gram: posting_params[:gram], temperature: posting_params[:temperature], time: posting_params[:time], experience: posting_params[:experience], coment: posting_params[:coment], user_id: current_user.id)
   end
 
   def new
@@ -16,7 +16,7 @@ class PostingController < ApplicationController
 
   private
   def posting_params
-    params.permit(:category_id, :name, :gram, :temperature, :time, :experience, :coment)
+    params.permit(:category_id, :name, :gram, :temperature, :time, :experience, :coment, :user_id)
   end
 
 end
