2f281b46b9966020dfe5d8259ade51f51a6506de
SatoTakuya <satotakuya@nuc-1x-dhcp-172-19-36-156.kwansei.ac.jp>
2016-04-08 16:07:13 +0900

    poostings modelにintegerじゃないと投稿できないようにvalidattion付けた

--- teatree/app/models/posting.rb
+++ teatree/app/models/posting.rb
@@ -3,6 +3,9 @@ class Posting < ActiveRecord::Base
   has_many :reviews
   validates :category_id, presence: true
   validates :name, presence: true
+  validates :time, numericality: true, allow_blank: true
+  validates :gram, numericality: true, allow_blank: true
+  validates :temperature, numericality: true, allow_blank: true
 
   has_attached_file :image,
                     :styles => {medium:"500x500#", thumb:"300x300#"}
